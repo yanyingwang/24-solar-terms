0dbe7073eeccc168ee17416153be415a6a2ad7c9
Yanying Wang <yanyingwang1@gmail.com>
2020-05-29 18:27:15 +0800

    files

--- info.rkt
+++ info.rkt
@@ -1,8 +1,8 @@
 #lang info
 (define collection "24-solar-terms")
-(define deps '("base"))
+(define deps '("base at-exp"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/24-solar-terms.scrbl" ())))
-(define pkg-desc "Description Here")
+(define pkg-desc "24 solar terms")
 (define version "0.0")
-(define pkg-authors '(yanying))
+(define pkg-authors '("Yanying Wang"))
